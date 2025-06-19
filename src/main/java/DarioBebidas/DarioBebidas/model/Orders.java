f1ac4550cb5c3c90c5a8b7a49478275d439acc90
LucassVinicius <lucasaviniciusz333@gmail.com>
2025-06-19 17:00:33 -0300

    Atualizando horário dos pedidos /3

--- src/main/java/DarioBebidas/DarioBebidas/model/Orders.java
+++ src/main/java/DarioBebidas/DarioBebidas/model/Orders.java
@@ -5,6 +5,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 import lombok.*;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -20,20 +29,22 @@ public class Orders {
     private String paymentMethod;
     private String status;
     @JsonFormat(shape = JsonFormat.Shape.STRING)
-    private LocalDateTime orderTime;
+    private OffsetDateTime orderTime;
     @JsonFormat(shape = JsonFormat.Shape.STRING)
-    private LocalDateTime estimatedDeliveryTime;
+    private OffsetDateTime estimatedDeliveryTime;
     private String deliveryAddress;
     private String phone;
-
     public Orders(List<Drink> drinks, String paymentMethod, String deliveryAddress, String phone) {
         this.drinks = drinks;
         this.paymentMethod = paymentMethod;
         this.deliveryAddress = deliveryAddress;
         this.status = "AGUARDANDO";
-        this.orderTime = LocalDateTime.now();
+
+        ZoneId saoPauloZone = ZoneId.of("America/Sao_Paulo");
+        this.orderTime = OffsetDateTime.now(saoPauloZone);
         this.estimatedDeliveryTime = this.orderTime.plusMinutes(30);
         this.phone = phone;
     }
 }
+
 
